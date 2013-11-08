e38ddd6a46549af24e223c52450c819c66d42ec9
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-11-08 15:27:59 -0800

    Add dependency required if one is running JDK6

--- src/clj-jvm/project.clj
+++ src/clj-jvm/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
                  [org.clojure/tools.reader "0.7.10"]]
   :main generator.core)
