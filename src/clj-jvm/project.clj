0d6b9d4a6df222f000e7417462a04a1f5b43cfb7
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-10-12 07:25:29 -0700

    Update versions of library deps to most recent versions
    ... except for one where there was a more recent SNAPSHOT version
    available, but I didn't want to use that one.

--- src/clj-jvm/project.clj
+++ src/clj-jvm/project.clj
@@ -3,17 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
-                 [org.clojure/core.async "0.3.465"]
-                 [org.clojure/data.priority-map "0.0.7"]
-                 [org.clojure/data.avl "0.0.17"]
+  :dependencies [[org.clojure/clojure "1.10.0-RC1"]
+                 ;;[org.clojure/clojure "1.9.0"]
+                 ;;[org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
+                 [org.clojure/core.async "0.4.474"]
+                 [org.clojure/data.priority-map "0.0.10"]
+                 [org.clojure/data.avl "0.0.18"]
                  [org.clojure/data.int-map "0.2.4"]
-                 [org.clojure/tools.reader "1.1.1"]
+                 [org.clojure/tools.reader "1.3.0"]
                  [org.flatland/ordered "1.5.6"]
                  [org.flatland/useful "0.11.5"]
-                 [org.clojure/core.rrb-vector "0.0.11"]
-                 [org.clojure-grimoire/lib-grimoire "0.10.9"]
+                 [org.clojure/core.rrb-vector "0.0.13"]
+                 [org.clojure-grimoire/lib-grimoire "0.10.10"]
                  [com.cemerick/url "0.1.1"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [org.clojure/test.check "0.10.0-alpha3"]]
   :main generator.core)
